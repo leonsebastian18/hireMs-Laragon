b5ae4210c312d8efc34a4dd7ddf9f0aef4067c01
Sebastian Leon <sebastianleonvillarreal@gmail.com>
2025-08-10 21:12:55 -0500

    Se crearon los layouts para el navbar se integro con el breeze

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,38 +1,44 @@
 {{-- resources/views/partials/navbar.blade.php --}}
-<header class="bg-white shadow-md header-container">
-    <div class="container mx-auto flex justify-between items-center p-4">
-        {{-- Logo --}}
-        <a href="{{ url('/') }}">
-            <img src="{{ asset('imagenes/logo_mobile.JPG') }}"
-                 alt="Logo-mobile"
-                 class="w-12 h-14 md:hidden rounded-lg" />
-            <img src="{{ asset('imagenes/logo.JPG') }}"
-                 alt="logo-desktop"
-                 class="hidden md:block static -mt-9 -mb-10 w-20" />
-        </a>
 
-        {{-- Navegación --}}
-        <nav>
-            <ul class="flex space-x-4">
-                @guest
-                    <li><a href="{{ route('login') }}" class="text-white hover:opacity-80">Login</a></li>
-                    <li><a href="{{ route('register') }}" class="text-white hover:opacity-80">Registrarse</a></li>
-                @endguest
+        <div class="container mx-auto flex justify-between items-center p-4">
+            <a href="./index.html">
+                <img src="../imagenes/logo_mobile.JPG" alt="Logo-mobile" class="w-12 h-14 md:hidden rounded-lg"/>
+                <img src="../imagenes/logo.JPG" alt="logo-desktop" class="hidden md:block static -mt-9 -mb-10  w-20">
+            </a>
 
-                @auth
-                    <li><a href="{{ route('dashboard') }}" class="text-white hover:opacity-80">Dashboard</a></li>
-                    <li>
-                        <form action="{{ route('logout') }}" method="POST" class="inline">
-                            @csrf
-                            <button type="submit" class="text-white hover:opacity-80">Cerrar Sesión</button>
-                        </form>
-                    </li>
-                @endauth
 
-                <li><a href="{{ url('/product') }}" class="text-white hover:opacity-80">Product</a></li>
-                <li><a href="{{ url('/prices') }}" class="text-white hover:opacity-80">Prices</a></li>
-                <li><a href="{{ url('/resources') }}" class="text-white hover:opacity-80">Resources</a></li>
-            </ul>
-        </nav>
-    </div>
-</header>
+            <nav>
+                <ul class="flex space-x-4">
+                    <li><a href="#" class="text-white hover:opacity-80">Product</a></li>
+                    <li><a href="#" class="text-white hover:opacity-80">Prices</a></li>
+                    <li><a href="#" class="text-white hover:opacity-80">Resources</a></li>
+                    @if (Route::has('login'))
+                <nav class="flex items-center justify-end gap-4">
+                    @auth
+                        <a
+                            href="{{ url('/dashboard') }}"
+                            class="inline-block px-5 py-1.5 border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] rounded-sm text-sm leading-normal"
+                        >
+                            Dashboard
+                        </a>
+                    @else
+                        <a
+                            href="{{ route('login') }}"
+                            class="inline-block px-5 py-1.5 text-[#FFFF] hover:opacity-80 border border-transparent hover:border-[#19140035] rounded-sm text-sm leading-normal"
+                        >
+                            Log in
+                        </a>
+
+                        @if (Route::has('register'))
+                            <a
+                                href="{{ route('register') }}"
+                                class="inline-block px-5 py-1.5 border-[#FFFF] hover:border-[#1915014a] border text-[#FFFF] hover:opacity-80 rounded-sm text-sm leading-normal">
+                                Register
+                            </a>
+                        @endif
+                    @endauth
+                </nav>
+            @endif
+                </ul>
+            </nav>
+        </div>
